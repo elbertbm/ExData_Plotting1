a51432a8d0c3a235bb36bf280e9781e410d48b29
elbertbm <david_w0123@yahoo.com.tw>
2015-07-13 00:27:38 +0800

    Create plot1.r

---
+++ plot1.r
@@ -0,0 +1,14 @@
+power<-read.table("./household_power_consumption.txt",sep=";",header=T)  #read the table
+#change Date and time cloumn to POSIXlt fromat
+power$Datetime<-paste(power$Date,power$Time)
+power$Datetime<-as.POSIXlt(strptime(power$Datetime,"%d/%m/%Y %H:%M:%S"))
+format(power$Timen,"%H:%M:%S")
+# select data in "2007-02-01" ,"2007-02-02"
+start<-as.Date("2007-02-01")
+end<-as.Date("2007-02-02")
+range <- seq(start,end,"day")
+sub<-power[as.Date(strptime(power$Date,"%d/%m/%Y"))==range,]
+
+png("./plot1.png",width = 480, height = 480)
+hist(as.numeric(sub$Global_active_power)/1000,col="red",breaks=12,xlab="Global active power(kilowatts)",main="Global active power")
+dev.off()
